098791d8cf4b06f7cd642fde6ed3be214949bc96
normy17 <normy17@mail.ru>
2024-01-11 20:52:17 +0300

    Регистрация и авторизация

--- project/urls.py
+++ project/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include("web.urls")),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from web.forms import *
+from web.models import *
 
 
+@login_required
 def main_view(request):
     return render(request, "web/main.html")
+
+
+def registration_view(request):
+    form = RegistrationForm()
+    is_success = False
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            user = User(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+            )
+
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            is_success = True
+    return render(request, 'web/registration.html', {
+        'form': form,
+        'is_success': is_success
+    })
+
+
+def auth_view(request):
+    form = AuthForm()
+    if request.method == 'POST':
+        form = AuthForm(data=request.POST)
+        if form.is_valid():
+            user = authenticate(**form.cleaned_data)
+            if user is None:
+                form.add_error(None, "Неправильный логин или пароль")
+            else:
+                login(request, user)
+                return redirect('main')
+    return render(request, 'web/auth.html', {
+        'form': form
+    })
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('main')
